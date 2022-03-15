a5570910bde9bce53a8ba8d2d33fda7f27f2a8ff
jihwan kim <cuchoco@naver.com>
2022-03-15 13:32:56 +0900

    Floyd

--- 07_shortest_route/03_Floyd-warshall.py
+++ 07_shortest_route/03_Floyd-warshall.py
@@ -5,3 +5,44 @@
 INF = int(1e9)
 n = 4 
 m = 7
+graph = [[INF]*(n+1) for _ in range(n+1)]
+edge = [
+    [1,2,4],
+    [1,4,6],
+    [2,1,3],
+    [2,3,7],
+    [3,1,5],
+    [3,4,4],
+    [4,3,2]
+]
+
+# 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        if a==b:
+            graph[a][b] = 0
+
+
+# 각 간선의 정보 graph에 대입.
+for i in range(m):
+    a, b, c = edge[i]
+    graph[a][b] = c
+
+# 점화식에 따라 플로이스 워셜 알고리즘 수행.
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+# 수행 결과 출력
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print("INF", end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
+
+
+
