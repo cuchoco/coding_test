840636df8fbbd211f4a11511da47494557be6968
Kim Jihwan <cuchoco@naver.com>
2022-03-20 01:33:36 +0900

    heap

--- programmers/heap_01_spicy.py
+++ programmers/heap_01_spicy.py
@@ -55,3 +55,23 @@ def solution(scoville, K):
         return -1
     else:
         return answer
+
+
+############ 1등 풀이 ##############
+import heapq as hq
+
+def solution(scoville, K):
+
+    hq.heapify(scoville)  ## heapify 로 변경
+    answer = 0
+    while True:
+        first = hq.heappop(scoville)
+        if first >= K:
+            break
+        if len(scoville) == 0:
+            return -1
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
+    return answer
